b2dc6fc429e9d676b7203ff9b52db423a1dc0d2a
deybyr647 <deybyr647@gmail.com>
2021-09-06 18:20:40 -0400

    Minor changes

--- pages/admin/addProject.tsx
+++ pages/admin/addProject.tsx
@@ -19,6 +19,18 @@ import { sendData } from "../../components/admin/api";
 import db from "../../components/admin/firebaseConfig";
 
 const AddProjectContent = () => {
+  const [name, setName] = useState("");
+  const [shortDesc, setShortDesc] = useState("");
+
+  const formChangeHandler = (e) => {
+    e.preventDefault();
+    const { id, value } = e.currentTarget;
+
+    if (id == "projectName") setName(value);
+    else if (id == "shortDesc") setShortDesc(value);
+    console.log(value);
+  };
+
   return (
     <Container fluid>
       <Row className={"p-0"}>
@@ -28,9 +40,39 @@ const AddProjectContent = () => {
 
         <Col xs={10} sm={11} className={"p-0 mt-4"}>
           <Container fluid>
-            <Jumbotron>
-              <h1>Add New Project!</h1>
-            </Jumbotron>
+            <Row>
+              <Col>
+                <Jumbotron className={"text-center"}>
+                  <h1>New Project</h1>
+                </Jumbotron>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col>
+                <Form>
+                  <Form.Group controlId={"projectName"}>
+                    <Form.Label>Project Name</Form.Label>
+                    <Form.Control
+                      type={"text"}
+                      placeholder={"Enter Project Name..."}
+                      value={name}
+                      onChange={formChangeHandler}
+                    />
+                  </Form.Group>
+
+                  <Form.Group controlId={"shortDesc"}>
+                    <Form.Label>Short Description</Form.Label>
+                    <Form.Control
+                      type={"text"}
+                      placeholder={"Enter Short Description..."}
+                      value={shortDesc}
+                      onChange={formChangeHandler}
+                    />
+                  </Form.Group>
+                </Form>
+              </Col>
+            </Row>
           </Container>
 
           <Footer />
