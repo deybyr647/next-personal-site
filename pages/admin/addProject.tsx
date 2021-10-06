74a04e68650834d4da995b86eba2145b2868b219
deybyr647 <deybyr647@gmail.com>
2021-10-06 16:22:24 -0400

    Finished UI aspects of addProject page

--- pages/admin/addProject.tsx
+++ pages/admin/addProject.tsx
@@ -23,16 +23,27 @@ const AddProjectContent = () => {
     backgroundColor: "#f9f9fa",
   };
 
-  const [name, setName] = useState("");
+  const [tagline, setTagline] = useState("");
   const [shortDesc, setShortDesc] = useState("");
+  const [techstack, setTechstack] = useState([]);
+  const [longDesc, setLongDesc] = useState("");
+  const [name, setName] = useState("");
+  const [demolink, setDemoLink] = useState("");
+  const [ghLink, setGhLink] = useState("");
 
-  const formChangeHandler = (e) => {
+  const formChangehandler = (e) => {
     e.preventDefault();
     const { id, value } = e.currentTarget;
 
-    if (id == "projectName") setName(value);
-    else if (id == "shortDesc") setShortDesc(value);
-    console.log(value);
+    if (id == "projectDesc") setLongDesc(value);
+    else if (id == "projectName") setName(value);
+    else if (id == "projectTechStack") setTechstack(value.split(","));
+    else if (id == "projectShortDesc") setShortDesc(value);
+    else if (id == "projectGithub") setGhLink(value);
+    else if (id == "demoLink") setDemoLink(value);
+    else if (id == "projectTagline") setTagline(value);
+
+    console.log(id, value);
   };
 
   return (
@@ -62,16 +73,29 @@ const AddProjectContent = () => {
                         <Form.Control type={"file"} />
                       </Form.Group>
 
-                      <Form.Group controlId={"projectDescription"}>
-                        <Form.Label>Project Description (short)</Form.Label>
+                      <Form.Group controlId={"projectDesc"}>
+                        <Form.Label>Project Description (long)</Form.Label>
                         <Form.Control
                           as={"textarea"}
-                          rows={5}
-                          className={"mb-3"}
+                          rows={10}
+                          value={longDesc}
+                          onChange={formChangehandler}
                         />
+                      </Form.Group>
 
-                        <Form.Label>Project Description (long)</Form.Label>
-                        <Form.Control as={"textarea"} rows={15} />
+                      <Form.Group
+                        className={"mb-3"}
+                        controlId={"projectShortDesc"}
+                      >
+                        <Form.Label>Project Description (short)</Form.Label>
+                        <Form.Control
+                          type={"text"}
+                          placeholder={"Short description..."}
+                          value={shortDesc}
+                          as={"textarea"}
+                          rows={5}
+                          onChange={formChangehandler}
+                        />
                       </Form.Group>
                     </Form>
                   </Card.Body>
@@ -91,6 +115,8 @@ const AddProjectContent = () => {
                       <Form.Control
                         type={"text"}
                         placeholder={"Project Name..."}
+                        value={name}
+                        onChange={formChangehandler}
                       />
                     </Form.Group>
 
@@ -99,6 +125,8 @@ const AddProjectContent = () => {
                       <Form.Control
                         type={"text"}
                         placeholder={"Project Tagline..."}
+                        value={tagline}
+                        onChange={formChangehandler}
                       />
                     </Form.Group>
                   </Form>
@@ -107,7 +135,58 @@ const AddProjectContent = () => {
                 </Jumbotron>
               </Col>
 
-              <Col md={12} lg={4} xl={3}></Col>
+              <Col md={12} lg={4} xl={3}>
+                <Card style={styling} className={"mb-4"}>
+                  <Card.Body>
+                    <Card.Title>Project Links</Card.Title>
+                    <Form>
+                      <Form.Group controlId={"demoLink"} className={"mb-3"}>
+                        <Form.Label>Project Demo Link</Form.Label>
+                        <Form.Control
+                          type={"text"}
+                          placeholder={"Demo Link..."}
+                          value={demolink}
+                          as={"textarea"}
+                          rows={5}
+                          onChange={formChangehandler}
+                        />
+                      </Form.Group>
+
+                      <Form.Group
+                        className={"mb-3"}
+                        controlId={"projectGithub"}
+                      >
+                        <Form.Label>Project GitHub Repository</Form.Label>
+                        <Form.Control
+                          type={"text"}
+                          placeholder={"GitHub Link..."}
+                          value={ghLink}
+                          as={"textarea"}
+                          rows={5}
+                          onChange={formChangehandler}
+                        />
+                      </Form.Group>
+
+                      <Form.Group
+                        className={"mb-3"}
+                        controlId={"projectTechStack"}
+                      >
+                        <Form.Label>Project Tech Stack List</Form.Label>
+                        <Form.Control
+                          type={"text"}
+                          placeholder={
+                            "Enter tech used (e.g. ReactJS,NextJS,etc.)..."
+                          }
+                          value={techstack}
+                          as={"textarea"}
+                          rows={5}
+                          onChange={formChangehandler}
+                        />
+                      </Form.Group>
+                    </Form>
+                  </Card.Body>
+                </Card>
+              </Col>
             </Row>
           </Container>
 
