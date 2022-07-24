47ca111c86fbee9441c2798580de15e3eecaa0b8
deybyr647 <deybyr647@gmail.com>
2022-07-24 15:53:08 -0400

    Changed font from Montserrat to Nunito

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,9 +10,15 @@ class MyDocument extends Document {
     return (
       <Html lang={"en"}>
         <Head>
+          <link rel={"preconnect"} href={"https://fonts.googleapis.com"} />
+          <link
+            rel={"preconnect"}
+            href={"https://fonts.gstatic.com"}
+            crossOrigin
+          />
           <link
             href={
-              "https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
+              "https://fonts.googleapis.com/css2?family=Nunito&display=swap"
             }
             rel={"stylesheet"}
           />
