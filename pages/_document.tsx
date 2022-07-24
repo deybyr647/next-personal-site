6a6d4cb464046c1a75974ac2f317cc8e191ad684
deybyr647 <deybyr647@gmail.com>
2022-07-24 16:04:23 -0400

    Minor fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,7 +14,7 @@ class MyDocument extends Document {
           <link
             rel={"preconnect"}
             href={"https://fonts.gstatic.com"}
-            crossOrigin
+            crossOrigin={"anonymous"}
           />
           <link
             href={
